0bc4e3d601dd12b582392470aa3decca713dc0b1
idamachmadfaizin <idamachmadfaizin@gmail.com>
2019-05-31 15:15:01 +0700

    add restaurant seeder

---
+++ database/seeds/RestaurantTableSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Restaurant;
+
+class RestaurantTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(Restaurant::class, 5)->create();
+    }
+}
